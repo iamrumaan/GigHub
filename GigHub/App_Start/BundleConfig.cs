7ccb8ba55f3de5c9dc826047a0688056e0123810
Mohammad Ruman Ansari <mohammad-ruman.ansari@capgemini.com>
2019-09-16 15:08:02 +0530

    Implement view gigs I'm attending

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace GigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }

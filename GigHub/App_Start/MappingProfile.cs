2a4799f44392a5b791a2fa30b111da89776a1650
Mohammad Ruman Ansari <mohammad-ruman.ansari@capgemini.com>
2019-09-17 19:03:00 +0530

    Implement Api for notification.

---
+++ GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using GigHub.Controllers.Api;
+using GigHub.Models;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        protected readonly IMapper _mapper;
+
+        public MappingProfile()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ApplicationUser, UserDto>();
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
+            });
+
+            _mapper = config.CreateMapper();
+        }
+    }
+}
---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId && !un.IsRead)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ApplicationUser, UserDto>();
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
+            });
+
+            var mapper = config.CreateMapper();
+            var dest = new Collection<NotificationDto>();
+            foreach (var notification in notifications)
+            {
+                var src = mapper.Map<Notification, NotificationDto>(notification);
+                dest.Add(src);
+            }
+
+
+            return dest;
+        }
+    }
+}
